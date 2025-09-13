baff052443099aec125855b7dba7224fa0a7c45b
Shayne Meyer <shayne.meyer@gmail.com>
2025-09-13 07:57:06 -0700

    added seller table to database

--- app/database/models.py
+++ app/database/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from enum import Enum
 
+from pydantic import EmailStr
 from sqlmodel import Field, SQLModel
 
 
@@ -21,3 +22,11 @@ class Shipment(SQLModel, table=True):
     destination: int
     status: ShipmentStatus
     estimated_delivery: datetime
+
+class Seller(SQLModel, table = True):
+
+    id: int = Field(default=None, primary_key=True)
+    name: str
+    
+    email: EmailStr
+    password_hash: str
