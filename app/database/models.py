a1194c8ac693836e98b0a918d6728e4fcdd7f895
Shayne Meyer <shayne.meyer@gmail.com>
2025-09-22 19:02:13 -0700

    added shipment_events table

--- app/database/models.py
+++ app/database/models.py
@@ -31,7 +31,11 @@ class Shipment(SQLModel, table=True):
     content: str
     weight: float = Field(le=25)
     destination: int
-    status: ShipmentStatus
+
+    timeline: list["ShipmentEvent"] = Relationship(
+        back_populates="shipment", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
     estimated_delivery: datetime
 
     seller_id: UUID = Field(foreign_key="seller.id")
@@ -42,6 +46,28 @@ class Shipment(SQLModel, table=True):
     delivery_partner_id: UUID = Field(foreign_key="delivery_partner.id")
     delivery_partner: "DeliveryPartner" = Relationship(
         back_populates="shipments", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+
+class ShipmentEvent(SQLModel, table=True):
+    __tablename__ = "shipment_event"
+
+    id: UUID = Field(sa_column=Column(postgresql.UUID, default=uuid4, primary_key=True))
+
+    created_at: datetime = Field(
+        sa_column=Column(
+            postgresql.TIMESTAMP,
+            default=datetime.now,
+        )
+    )
+
+    location: int
+    status: ShipmentStatus
+    description: str | None = Field(default=None)
+
+    shipment_id: UUID = Field(foreign_key="shipment.id")
+    shipment: Shipment = Relationship(
+        back_populates="timeline", sa_relationship_kwargs={"lazy": "selectin"}
     )
 
 
@@ -63,6 +89,9 @@ class Seller(User, table=True):
             default=datetime.now,
         )
     )
+
+    address: str | None = Field(default=None)
+    zip_code: int | None = Field(default=None)
 
     shipments: list[Shipment] = Relationship(
         back_populates="seller", sa_relationship_kwargs={"lazy": "selectin"}
